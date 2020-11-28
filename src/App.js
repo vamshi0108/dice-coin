a1acb48372537ade52a725b80feb8f33af6fb782
vamshi.nagulapally <vamshi.nagulapally@comviva.com>
2020-11-29 01:32:49 +0530

    Rotating dice added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import classes from "./App.module.css";
 import "./App.module.css";
-import React, { useState } from "react";
+import React, { Component } from "react";
 import { motion } from "framer-motion";
 
 const parentMotion = {
@@ -17,8 +17,8 @@ const diceVariants = {
   visible: {
     rotate: 360,
     transition: {
-      // yoyo: 10,
-      ease: "easeInOut",
+      yoyo: 7,
+      duration: 0.2,
     },
   },
   hover: {
@@ -65,49 +65,68 @@ const btnVariants = {
   },
 };
 
-function App() {
-  return (
-    <motion.div
-      variants={parentMotion}
-      initial="hidden"
-      animate="visible"
-      className={classes.App}
-    >
+class App extends Component {
+  state = {
+    dummyDice: true,
+  };
+
+  trigger = () => {
+    this.setState({ dummyDice: !this.state.dummyDice });
+  };
+
+  componentDidUpdate() {
+    if (!this.state.dummyDice) {
+      this.setState({ dummyDice: !this.state.dummyDice });
+    }
+  }
+
+  render() {
+    return (
       <motion.div
-        variants={bgdiceVariants}
-        className={classes.bgdice1}
-      ></motion.div>
-      <motion.div
-        variants={bgdiceVariants}
-        className={classes.bgdice2}
-      ></motion.div>
-      <motion.div
-        variants={bgdiceVariants}
-        className={classes.bgdice3}
-      ></motion.div>
-      <motion.div
-        variants={bgdiceVariants}
-        className={classes.bgdice4}
-      ></motion.div>
-      {/* <motion.h3 className={classes.logo} animate={{ x: -470, y: -7 }}>
+        variants={parentMotion}
+        initial="hidden"
+        animate="visible"
+        className={classes.App}
+      >
+        <motion.div
+          variants={bgdiceVariants}
+          className={classes.bgdice1}
+        ></motion.div>
+        <motion.div
+          variants={bgdiceVariants}
+          className={classes.bgdice2}
+        ></motion.div>
+        <motion.div
+          variants={bgdiceVariants}
+          className={classes.bgdice3}
+        ></motion.div>
+        <motion.div
+          variants={bgdiceVariants}
+          className={classes.bgdice4}
+        ></motion.div>
+        {/* <motion.h3 className={classes.logo} animate={{ x: -470, y: -7 }}>
         Welcome to Dice-Coin
       </motion.h3> */}
-      <motion.div
-        variants={diceVariants}
-        whileTap="tap"
-        whileHover="hover"
-        className={classes.dice}
-      ></motion.div>
-      <motion.button
-        variants={btnVariants}
-        whileTap="tap"
-        whileHover="hover"
-        className={classes.rollBtn}
-      >
-        Click here
-      </motion.button>
-    </motion.div>
-  );
+        {this.state.dummyDice && (
+          <motion.div
+            variants={diceVariants}
+            whileTap="tap"
+            whileHover="hover"
+            className={classes.dice}
+          ></motion.div>
+        )}
+        <motion.button
+          variants={btnVariants}
+          whileTap="tap"
+          whileHover="hover"
+          className={classes.rollBtn}
+          onClick={this.trigger}
+        >
+          Roll Dice
+        </motion.button>
+      </motion.div>
+    );
+  }
 }
 
 export default App;
